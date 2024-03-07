a4cacd1b37ae2ce6af45c8088eed7e8d0ab65da3
Alexander Decker <deckeraga@gmail.com>
2024-03-06 17:26:34 -0800

    Basic Dark Mode

--- src/components/home/FeatureCard.jsx
+++ src/components/home/FeatureCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function FeatureCard(props) {
   return (
-    <div className='m-4 flex w-80 grow flex-col items-center border-2 p-8 sm:grow-0'>
+    <div className='m-4 flex w-80 grow flex-col items-center rounded-lg border-2 p-8 sm:grow-0 dark:border-gray-600'>
       <img className='mb-4 w-48' src={props.imgPath} />
       <div>
         <div className='mb-4 text-2xl font-bold text-brand-primary'>
